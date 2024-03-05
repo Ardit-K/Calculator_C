dbac57cbba890dd1ed7d62a1de7de73a477d0131
ardit <arditkoti7192@gmail.com>
2024-03-05 18:27:34 -0500

    Started main.cpp and added comments.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*
+File: main.cpp
+Author: Ardit Koti
+Description: This is the main entry point for the calculator program.
+It handles operation calls and I/O.
+*/
+int main(){
+    return 0;
+}
